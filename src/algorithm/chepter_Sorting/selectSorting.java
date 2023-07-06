136d6b5bc1acd6e7a9a5e410deeaf24adb124981
chowg <devkiwancho@gmail.com>
2023-07-06 23:15:57 +0900

    sorting 알고리즘 - 선택정렬, 버블정렬, 삽입정렬

--- src/algorithm/chepter_Sorting/selectSorting.java
+++ src/algorithm/chepter_Sorting/selectSorting.java
@@ -9,11 +9,29 @@ public class selectSorting {
         int n=kb.nextInt();
         int[] arr=new int[n];
         for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        for(int x : solution(n, arr)) System.out.print(x+" ");
+        for(int x : solutionSort(n, arr)) System.out.print(x+" ");
     }
 
     public static int[] solution(int n, int[] arr) {
         Arrays.sort(arr);
+
+        return arr;
+    }
+
+    public static int[] solutionSort(int n, int[] arr) {
+        for (int i = 0; i < n-1; i++) {
+            int idx = i;
+
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
 
         return arr;
     }
---
+++ src/algorithm/chepter_Sorting/sortingBubble.java
@@ -0,0 +1,28 @@
+package algorithm.chepter_Sorting;
+
+import java.util.Scanner;
+
+public class sortingBubble {
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        for(int x : solution(n, arr)) System.out.print(x+" ");
+    }
+
+    public static int[] solution(int n, int[] arr) {
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
